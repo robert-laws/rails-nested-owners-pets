8c94614fc883f4289204a3625595860d4aace1b3
Robert Laws <roblaws@me.com>
2018-02-19 16:48:02 +0300

    add namespacing with routing and module

---
+++ app/controllers/admin/stats_controller.rb
@@ -0,0 +1,5 @@
+class Admin::StatsController < ApplicationController
+  def index
+    @welcome = "Welcome to the Stats Area"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     resources :pets, only: [:index, :show, :new, :create]
   end
 
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   root 'owners#index'
 end
